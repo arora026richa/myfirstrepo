8413d886e89988769f5f87b117b019d5d988d665
arora026richa <142480947+arora026richa@users.noreply.github.com>
2025-04-16 13:19:19 +0530

    Update email_test.py

--- email_test.py
+++ email_test.py
@@ -1,2 +1,2 @@
 print("Testing Jenkins email alerts - this build should fail intentionally")
-prin("This is an intentional typo to fail the build")  # 'prin' instead of 'print' 
+print("This is an intentional typo to fail the build")  # corrected 
